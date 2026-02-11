869b9015b2e9849ac415a71ec62b4767a0a7fb2d
Jonah Min <jonahm2472@gmail.com>
2026-02-10 20:31:57 -0500

    Add links and more info for sightings

--- src/BirdsList.tsx
+++ src/BirdsList.tsx
@@ -7,22 +7,44 @@ type BirdsListProps = {
 
 export default function BirdsList({ birds }: BirdsListProps) {
   const birdCountsByName = birds ? birds.reduce((acc, bird) => {
-    const { comName, howMany } = bird;
-    if (!acc[comName]) acc[comName] = 0;
-    acc[comName] += howMany;
+    const { comName, howMany, speciesCode, subId } = bird;
+
+    if (!acc[comName]) {
+      acc[comName] = {
+        count: 0,
+        code: speciesCode,
+        checklist: subId
+      };
+    }
+
+    acc[comName].count += howMany;
     return acc;
-  }, {} as Record<string, number>) : {};
+  }, {} as Record<string, { count: number; code: string; checklist: string; }>) : {};
 
   return <div className={`sightings ${birds ? 'selected' : ''}`}>
     {birds &&
-      <div style={{ paddingLeft: '4%', paddingTop: '4%' }}>
-        {Object.entries(birdCountsByName).map(([birdName, count]) => {
+      <div style={{ paddingLeft: '4%', paddingTop: '4%', display: 'flex', flexDirection: 'column' }}>
+        <h1 className="bird-list-header">{birds[0].locName}</h1>
+        {Object.entries(birdCountsByName).map(([birdName, info]) => {
+          const { count, code, checklist } = info;
+
+          const ebirdLink = `https://ebird.org/species/${code}`;
+          const checklistLink = `https://ebird.org/checklist/${checklist}`;
+          const photosLink = `https://search.macaulaylibrary.org/catalog?taxonCode=${code}&sort=rating_rank_desc`;
+
           return <Fragment key={birdName}>
-            <span style={{ textAlign: "left", fontSize: '20px', fontWeight: 'bold' }}>{birdName}</span>
-            <br />
-            <span>count: {count}</span>
-            <br />
-            <br />
+            <span className="bird-sighting">
+              <span className="count">{count} x</span>
+              <strong>
+                <a className="bird-name" href={ebirdLink} target='_blank' >{birdName}</a>
+              </strong>
+              <br />
+              <div className="extra-info">
+                <a href={checklistLink} target='_blank' >Checklist</a>
+                <br />
+                <a href={photosLink} target='_blank' >Macaulay library</a>
+              </div>
+            </span>
           </Fragment>;
         })}
 
